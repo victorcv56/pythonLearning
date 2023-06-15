253f01b2972dfcee384dae87b677d2b2e98a3886
victorcv56 <victorcv56@gmail.com>
2023-06-14 17:04:40 -0700

    Add to list function finished

--- storeProject/json_practice.py
+++ storeProject/json_practice.py
@@ -1,40 +1,45 @@
 import json
 filename = 'test.json'
+games_catalogue = {}#empty dictionary for user to fill
 
-games_catalogue = {'Red Dead Redemption 2': {'cost':10.6, 'price':15},
-                   'FF7': {'cost':10.6, 'price':20},
-                   'Dragon Ball Kakarot': {'cost':10.6, 'price':15},
-                   'God of War': {'cost':10.6, 'price':15},
-                   'Greedfall': {'cost':10.6, 'price':12},
-                   'WWE 2k23': {'cost':10.6, 'price':13},
-                   'Re-Reckoning': {'cost':10.6, 'price':12}
-                   }
+def individual_price(lot_price, num_games):
+    """Calculate individual price for lots bought"""
+    price = lot_price / num_games
+    return price
 
 def retrieve_data(filepath):
+    """Access json data and sabe it to new list variable"""
     try:
         with open(filepath) as f:
             list = json.load(f)
-            
     except FileNotFoundError:
-            with open(filepath, 'w') as f:
-                json.dump(games_catalogue, f, sort_keys=True, indent= 3)#Learning python library
+        with open(filepath, 'w') as f:
+            json.dump(games_catalogue, f, sort_keys=True, indent= 3)#Learning python library
     return list
 
-def add_to_list(new_data, filepath):
-    """Want to create a funtion that will add to dictionary and add it to json"""
-    name = input("Name of game: ")
-    cost = input("Price of sale: ")
-    price = input("Cost of game: ")
-    
-    new_data[name] = {}
-    new_data[name]['cost'] = cost
-    new_data[name]['price'] = price
-    
-    json.dump(new_data, filepath)
-    print(new_data)
-    
+def dlls_to_peso(dlls):
+    """Convert dllr prices to pesos."""
 
+
+def add_to_list(game_list, filepath):
+    """Want to create a funtion that will add to dictionary and add it to json"""
+    # with open(filepath) as f:
+    #     game_list = json.load(f)
+    game_count = int(input("\nHow many games would you like to add? "))   
+    for i in range(game_count):
+        name = input("\nName of game: ")
+        cost = int(input("\nSelling for: "))
+        price = int(input("\nGame cost: "))
+    
+        game_list[name] = {}
+        game_list[name]['cost'] = cost
+        game_list[name]['price'] = price
+    
+    with open(filepath, 'w') as f:
+        json.dump(game_list, f, indent=3)
+    print(game_list)
+    
+lot1 = individual_price(62.94, 12)
+print(lot1)
 new_data = retrieve_data(filename)
-print(new_data)
-print(type(new_data))
 add_to_list(new_data, filename)
